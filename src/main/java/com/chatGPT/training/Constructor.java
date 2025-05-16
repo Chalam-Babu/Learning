a413fc6deb6215b5038c04f75f9dc10e27f53fac
Chalam Babu <chalamcherry28@gmail.com>
2025-05-16 19:52:20 +0530

    example of Constructor

---
+++ src/main/java/com/chatGPT/training/Constructor.java
@@ -0,0 +1,18 @@
+package com.chatGPT.training;
+class Person{
+    Person(){
+        System.out.println("Person is creatwed");
+    }
+
+}
+class Employee extends Person{
+    Employee(){
+        super();
+        System.out.println("Employee is created");
+    }
+}
+public class Constructor {
+    public static void main(String [] args){
+        Employee e = new Employee();
+    }
+}
