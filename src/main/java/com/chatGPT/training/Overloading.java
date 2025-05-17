82127520c82086b272c11e4c9f7f467781d17a17
Chalam Babu <chalamcherry28@gmail.com>
2025-05-17 12:17:03 +0530

    example of overloading

---
+++ src/main/java/com/chatGPT/training/Overloading.java
@@ -0,0 +1,31 @@
+package com.chatGPT.training;
+
+import java.util.Scanner;
+
+class Printer{
+    String print(String str){
+        return str;
+    }
+    int print(int number){
+        return number;
+    }
+
+     void print(String str, int number){
+        for(int i =0; i<number;i++){
+            System.out.println(i + " " + str);
+        }
+
+     }
+}
+
+public class Overloading {
+    public static void main(String [] args){
+
+        Printer p = new Printer();
+        System.out.println(p.print("Hello"));
+        System.out.println(p.print(2456565));
+        p.print("i am chalam", 3);
+
+    }
+
+}
