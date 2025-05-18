f5d3051a3808e86f75b343b6eb56233bc9bb2de9
Chalam Babu <chalamcherry28@gmail.com>
2025-05-18 14:23:55 +0530

    sample code for Test

---
+++ src/main/java/com/own/training/Addition.java
@@ -0,0 +1,14 @@
+package com.own.training;
+
+public class Addition {
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int sub(int a, int b){
+        return a-b;
+    }
+
+    public int isPositive(int n){
+        return n;
+    }
+}
---
+++ src/main/java/com/own/training/Calculator.java
@@ -0,0 +1,18 @@
+package com.own.training;
+
+public class Calculator {
+    public int add(int a , int b){
+        return a+b;
+    }
+
+    public int sub(int a, int b){
+        return a-b;
+    }
+
+    public int divide(int a, int b){
+        if(b==0){
+            throw new IllegalArgumentException("can not devided by 0");
+        }
+        return a/b;
+    }
+}
